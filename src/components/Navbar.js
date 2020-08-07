7d6ca88429ed0a8e64b234f4c467a642963600b6
Bruna <brunagenz92@gmail.com>
2020-08-07 18:16:28 -0300

    Update Navbar component to display logo and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Logo from '../images/logo2.png';
 
 const Navbar = () => {
+  const links = ['Projects', 'Skills', 'Contact']
 
+  return (
+    <nav className="Navbar"> 
+      <a href='logo' key='logo'>
+        <img src={Logo} alt="Bruna's logo" />
+      </a>
+      {links.map((link, index) => (
+        <a href={`#${link}`} key={index}>{link}</a>
+      ))}
+    </nav>
+    
+  )
 }
 
 export default Navbar;
