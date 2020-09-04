d79cfd8f1796a2051d56f151d58134c22f631470
Bruna <brunagenz92@gmail.com>
2020-09-04 13:00:50 -0300

    styles: edit styles of Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import Logo from '../images/logo2.png';
+import Logo from '../images/Bruna-logo.png';
 import '../styles/Navbar.scss';
 
 const Navbar = () => {
   const links = ['Projects', 'Skills', 'Contact']
 
   return (
-    <nav className="Navbar shadow"> 
+    <nav className="Navbar gradient-border"> 
       <a className="logo" href='logo' key='logo'>
         <img src={Logo} alt="Bruna's logo" />
       </a>
-      {links.map((link, index) => (
-        <a href={`#${link}`} key={index}>{link}</a>
-      ))}
+      <div className="navbar-links">
+        {links.map((link, index) => (
+          <a href={`#${link}`} key={index}>{link}</a>
+        ))}
+      </div>
     </nav>
     
   )
