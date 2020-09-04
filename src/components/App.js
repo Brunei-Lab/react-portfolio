c116b68dc8ebee2fe520898dd84d6253efa40971
Bruna <brunagenz92@gmail.com>
2020-09-04 16:43:37 -0300

    Styles: started using styles modules

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './Navbar';
 import Presentation from './Presentation';
 import ProjectsContainer from '../containers/ProjectsContainer';
+import Background from '../components/Background';
 
 function App() {
   return (
     <div className="App">
       < Navbar />
       < Presentation />
+      < Background background="watercolor-bg"/>
       < ProjectsContainer />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Logo from '../images/Bruna-logo.png';
-import '../styles/Navbar.scss';
+import styles from '../styles/Navbar.module.scss';
 
 const Navbar = () => {
   const links = ['Projects', 'Skills', 'Contact']
 
   return (
-    <nav className="Navbar gradient-border"> 
-      <a className="logo" href='logo' key='logo'>
+    <nav className={`${styles.Navbar} ${styles["gradient-border"]}`}> 
+      <a className={styles.logo} href='logo' key='logo'>
         <img src={Logo} alt="Bruna's logo" />
       </a>
-      <div className="navbar-links">
+      <div className={styles["navbar-links"]}>
         {links.map((link, index) => (
           <a href={`#${link}`} key={index}>{link}</a>
         ))}
