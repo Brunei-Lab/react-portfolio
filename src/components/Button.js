f588c4f70fcb7bbb2b5cb6823e486cad6eaa0d5f
Bruna <brunagenz92@gmail.com>
2020-09-18 14:41:13 -0300

    Styles: update styles for presentation component

--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../styles/Button.module.scss';
-
-const Button = ({url, text}) => (
-  <a className={styles.Button} href={url}>{text}</a>
-)
-
-export default Button;
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import styles from '../styles/Presentation.module.scss';
 import Elements from '../components/Elements';
-import Button from './Button';
 
 const Presentation = () => (
   <div className={styles.Presentation}>
     <div className={styles.text}>
       <h1 className={styles.title}>Hello, I'm <span>Bruna</span>.</h1>
       <h3 className={styles.subtitle}>I'm a veterinarian turned <span>full-stack developer</span> based in Halifax, Canada.</h3>
-      <h3 className={styles.subtitle}>I love podcasts, books, and elegant UX/UI.</h3>
-      <h4 className={styles.subtitle}>Open to <span className={styles.black}>freelance</span> and <span className={styles.black}>remote work</span>.</h4>
-      <Button url='#Projects' text='See my projects' />
+      <h3 className={styles.subtitle}>I love podcasts, books, and elegant UX/UI. Also, I'm a graphic design enthusiast.</h3>
+      <h3 className={styles.subtitle}>Open to <span className={styles.black}>freelance</span> and <span className={styles.black}>remote work</span>.</h3>
+      <a className={styles.button} href='#Projects'>See my projects</a>
     </div>
     <div className={styles["elements-container"]}>
       <Elements />
